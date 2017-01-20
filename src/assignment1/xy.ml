6cd91da30f073ec2ab4a48b3c38b3efa6d7a9774
anwesht <anwesh.tuladhar@gmail.com>
2017-01-19 20:06:55 -0500

    Commented code for xy.ml

--- src/assignment1/xy.ml
+++ src/assignment1/xy.ml
@@ -41,6 +41,11 @@ fun printxy(P) =
           )
       );
 
+    (** print x then the y part of the polynomial
+      * @param x::xs => polynomial in x = inner int list
+      * @param e => exponent of x
+      * @param y => string representing the y portion of the polynomial
+      *)
     fun print_xWithy(nil, e, y:string) = print("")
       | print_xWithy(x::xs, 0, y:string) = (
           if x=0 then (
@@ -79,6 +84,7 @@ fun printxy(P) =
           print_xWithy(xs, e+1, y)
         );
 
+    (** Evaluates the y portion of the polynomial and calls print_xWithy *)
     fun print_xy(nil, ey) = (
           if ey=0 then print(Int.toString(0))
           else  print("")
@@ -101,17 +107,11 @@ fun printxy(P) =
   end;
     
 
-fun evalx(P, xVal) = 
-  let 
-    fun evalPower(base, 0) = 1
-      | evalPower(base, exp) = base * evalPower(base, exp - 1); 
-  
-    fun eval_x(nil, _, _) = 0
-      | eval_x(x::xs, xVal, exp) = (x * evalPower(xVal, exp)) + eval_x(xs, xVal, exp+1);
-  in 
-    eval_x(P, xVal, 0)
-  end;
-
+(** Evaluates the value of the polynomial, given the values of x and y
+  * @param P => int list list representation of polynomial in x and y
+  * @param xVal => value of x
+  * @param yVal => value of y
+  *)
 fun evalxy(P, xVal, yVal) = 
   let 
     fun evalPower(base, 0) = 1
@@ -128,60 +128,71 @@ fun evalxy(P, xVal, yVal) =
     eval_xy(P, xVal, yVal, 0)
   end;
   
+(** Evaluates the sum of two polynomials in x and y
+  * @param P => int list list representation of the first polynomial in x and y
+  * @param Q => int list list representation of the second polynomial in x and y
+  *)
 fun paddxy(P, nil) = P
   | paddxy(nil, Q) = Q
   | paddxy(p::ps, q::qs) = 
     let 
+      (** Helper function to sum the inner int lists *)
       fun paddx(P, nil) = P
       | paddx(nil, Q) = Q
       | paddx((p:int)::ps, q::qs) = (p + q)::paddx(ps, qs);
     in 
+      (** Recursively sum corresponding values of two polynomials *)
       paddx(p, q)::paddxy(ps, qs)
     end;
-    
+
+(** Evaluates the multiplication of two polynomials in x and y
+  * @param P => int list list representation of the first polynomial in x and y
+  * @param Q => int list list representation of the second polynomial in x and y
+  *)
 fun multxy(nil, _) = nil
   | multxy(xy::xys, mult) = 
     let
+      (** Helper function to multiply the inner int list with a scalar
+        * @param x::xs => int list (inner)
+        * @param mult => the scalar value
+        *)
       fun multx(nil, _) = nil
         | multx(x::xs, mult) = (x * mult)::multx(xs, mult)
     in
+      (* Multiply the head of the list (i.e. innerlist) with the scalar
+        and construct the int list list*)
       multx(xy, mult)::multxy(xys, mult)
     end
 
+(** Evaluates the multiplication of two polynomials in x and y
+  * @param P => int list list representation of the first polynomial in x and y
+  * @param Q => int list list representation of the second polynomial in x and y
+  *)
 fun pmultxy(_, nil) = nil
   | pmultxy(nil, _) = nil
   | pmultxy(P, q::qs) = 
     let 
+      (** Helper function to multiply the inner int lists.
+        * PQ = P*q + qs*x ==> qs*x is obtained by shifting to the right
+        *)
       fun pmult(P, nil) = nil
         | pmult(P, q::qs) = padd(smult(P, q), 0::pmult(P, qs));
 
+      (** Helper function to multiply the first polynomial in x and y 
+        * with an inner list.
+        * @param P => int list list (polynomial in x and y)
+        * @param q => int list (the inner int list)  
+        *)
       fun pmultx(P, nil) = P
         | pmultx(nil, _) = nil
         | pmultx((p:int list)::ps, q: int list) = 
+        (** Construct the intermediate product of polynomial P with the coefficient and x variable.
+          * This represents => P * q, where q = inner int list
+          *)
         pmult(p, q)::pmultx(ps, q);
     in
+      (** pmultx(P, q) returns P * q i.e. P * (only x part)
+        * Shifting to the right represents the multiplication by y.
+        *)
       paddxy(pmultx(P, q), nil::pmultxy(P, qs))
     end;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
