7d20f077e3c1bf3006a033a862271bf66f4af01e
anwesht <anwesh.tuladhar@gmail.com>
2017-02-26 12:36:08 -0500

    Working dirty uniquify

--- src/assignment3/as3.ml
+++ src/assignment3/as3.ml
@@ -67,7 +67,150 @@ fun sub (e:expr) x TrueExpr = TrueExpr
           (sub e x body)
         )
       else raise Capture;
+
+(*fun uniquifyVars expr = 
+  let     
+    fun uniquify TrueExpr _ = TrueExpr
+      | uniquify FalseExpr _ = FalseExpr
+      | uniquify (ie as IntExpr(_)) _ = ie
+      | uniquify (ve as VarExpr(_)) _ = ve
+      | uniquify (PlusExpr(left, right)) count = PlusExpr((sub e x left), (sub e x right))
+      | uniquify (LessExpr(left, right)) count = LessExpr((sub e x left), (sub e x right))
+      | uniquify (IfExpr(condition, thenBranch, elseBranch)) count = 
+          IfExpr((sub e x condition), (sub e x thenBranch), (sub e x elseBranch))
+      | uniquify (ApplyExpr(function, argument)) count = ApplyExpr((sub e x function), (sub e x argument))
+      | uniquify (fe as FunExpr(functionName, parameterName, parameterType, returnType, body)) count = 
+          if (functionName = x orelse parameterName = x) then fe
+          else if (not (isFV e functionName) andalso not (isFV e parameterName)) then
+            FunExpr(functionName, parameterName, parameterType, returnType, 
+              (sub e x body)
+            )
+          else raise Capture;
+  in 
+    uniquify expr 101
+  end;*)
       
+fun uniquifyVars expr = 
+  let
+    fun uniquify TrueExpr inScope count = (TrueExpr, count)
+      | uniquify FalseExpr inScope count = (FalseExpr, count)
+      | uniquify (ie as IntExpr(_)) inScope count = (ie, count)
+      | uniquify (VarExpr(v)) inScope count = 
+          let 
+            (*fun rename (v:string) inScope count = *)
+              (*if #1(#1 (inScope)) = v then
+                #2(#1 (inScope))
+              else if #1(#2 (inScope)) = v then
+                #2(#2 (inScope))
+              else "var_"^Int.toString(count)*)
+              
+                (*val (newVar, newCount, _) = foldl (fn ((old, new), (newVar, nc, isInScope)) => 
+                  if v = old andalso not isInScope then (new, nc, true) 
+                  else if isInScope then (newVar, nc, isInScope) 
+                  else (newVar, nc+1, isInScope)
+                ) 
+                (("var_"^Int.toString(count)), count, false) 
+                (inScope)*)
+                val (newVar, isInScope) = foldl (fn ((old, new), (newVar, iis)) => 
+                  (*if v = old andalso not isInScope then (new, true) else (newVar, isInScope)*)
+                  if v = old andalso not iis then (new, true) 
+                  else (newVar, iis)
+                ) 
+                (("var_"^Int.toString(count)), false) 
+                (inScope)
+              
+          in 
+            (*(VarExpr(rename v inScope count), count+1)*)
+            (VarExpr(newVar), if isInScope then count else count+1)
+          end
+      | uniquify (PlusExpr(left, right)) inScope count =
+          let 
+            val (leftVal, leftCount) = uniquify left inScope count
+            val (rightVal, rightCount) = uniquify right inScope leftCount
+          in
+            (*(PlusExpr(leftVal, right), count)*)
+            (PlusExpr(leftVal, rightVal), rightCount)
+          end
+      | uniquify (LessExpr(left, right)) inScope count = 
+          let 
+            val (leftVal, leftCount) = uniquify left inScope count
+            val (rightVal, rightCount) = uniquify right inScope leftCount
+          in
+            (LessExpr(leftVal, rightVal), rightCount)
+          end
+      | uniquify (IfExpr(condition, thenBranch, elseBranch)) inScope count =
+          let 
+            val (condVal, condCount) = uniquify condition inScope count
+            val (thenVal, thenCount) = uniquify thenBranch inScope condCount
+            val (elseVal, elseCount) = uniquify elseBranch inScope thenCount
+          in
+            (IfExpr(condVal, thenVal, elseVal), elseCount)
+          end
+      | uniquify (ApplyExpr(function, argument)) inScope count = 
+          let 
+            val (funVal, funCount) = uniquify function inScope count
+            val (argVal, argCount) = uniquify argument inScope funCount
+          in
+            (ApplyExpr(funVal, argVal), argCount)
+          end
+      | uniquify (fe as FunExpr(functionName, parameterName, parameterType, returnType, body)) inScope count = 
+          let 
+            (*val currentScopeList = ((functionName, "fun"^"_"^Int.toString(count)), (parameterName, "var"^"_"^Int.toString(count)))*)
+            val funPair = (functionName, "fun"^"_"^Int.toString(count))
+            val paramPair = (parameterName, "var"^"_"^Int.toString(count))
+
+            fun isIn _ nil = false
+              | isIn (x:string) ((y,_)::ys) = x=y orelse isIn x ys;
+
+            fun augmentScope nil currentScope = currentScope
+              | augmentScope ((p as (old, _))::xs) currentScope = 
+                  if isIn old currentScope then 
+                    augmentScope xs currentScope
+                  else 
+                    augmentScope xs (p::currentScope);
+
+            val currentScope = augmentScope inScope [funPair, paramPair]
+            val (newBody, bodyCount) = uniquify body currentScope (count+1)
+            (*val (newBody, bodyCount) = uniquify body [funPair, paramPair] (count+1)*)
+          in 
+            ( FunExpr(
+                #2 funPair,
+                #2 paramPair,
+                parameterType,
+                returnType,
+                newBody
+              ),
+              bodyCount
+            )
+          end
+  in 
+    #1(uniquify expr nil 1)
+  end;
+
+
+fun printExpr TrueExpr  = print("true ")
+  | printExpr FalseExpr = print("false ")
+  | printExpr (IntExpr(i)) = print(Int.toString(i)^" ")
+  | printExpr (VarExpr(v)) = print(v)
+  | printExpr (PlusExpr(left, right)) = (printExpr(left); print(" + "); printExpr(right))
+  | printExpr (LessExpr(left, right)) = (printExpr(left); print(" < "); printExpr(right))
+  | printExpr (IfExpr(condition, thenBranch, elseBranch)) = 
+    (
+      print("if ");
+      printExpr(condition); 
+      print(" then \n\t");
+      printExpr(thenBranch);
+      print("\n else \n\t");
+      printExpr(elseBranch)
+    )
+  | printExpr (ApplyExpr(function, argument)) = (printExpr(function); print( "( "); printExpr(argument); print(")"))
+  | printExpr (FunExpr(functionName, parameterName, parameterType, returnType, body)) =
+      (
+        print("\n(fun "^functionName^" ("^parameterName^") = \n\t");
+        printExpr(body);
+        print("\n)\n")
+      );
+
 
 
 
@@ -119,6 +262,22 @@ val f5 = FunExpr(
             VarExpr("z"),
             VarExpr("y")
           )
+        );
+
+val f6 = FunExpr(
+          "f", "x" , Int, Bool, 
+          ApplyExpr(
+            FunExpr(
+              "f", "x", Bool, Bool, 
+              ApplyExpr(VarExpr("f"), VarExpr("x"))
+            ),
+            IntExpr(5)
+          )
+        );
+
+val f7 = FunExpr(
+          "f", "x" , Int, Bool, 
+          ApplyExpr(VarExpr("f"), VarExpr("x"))
         );
 
 
