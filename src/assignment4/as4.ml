71f929dfe9834d6d4cb51501d058b602d4245658
anwesht <anwesh.tuladhar@gmail.com>
2017-03-12 20:10:47 -0400

    Working type checker.

--- src/assignment4/as4.ml
+++ src/assignment4/as4.ml
@@ -6,7 +6,7 @@
 
 use "as3_with_test.ml";
 
-fun tc expr = 
+(*fun tc expr = 
   let 
     fun typeExpr TrueExpr _ = SOME Bool
       | typeExpr FalseExpr _ = SOME Bool
@@ -32,4 +32,69 @@ fun tc expr =
       | typeExpr _ _ = NONE;
   in 
     typeExpr expr nil
+  end;*)
+
+fun tc expr = 
+  let 
+    fun typeExpr TrueExpr _ = SOME Bool
+      | typeExpr FalseExpr _ = SOME Bool
+      | typeExpr (IntExpr(_)) _ = SOME Int
+      | typeExpr (PlusExpr(l, r)) context = 
+          if (typeExpr l context) = SOME Int andalso (typeExpr r context) = SOME Int then
+            SOME Int
+          else 
+            NONE
+      | typeExpr (LessExpr(l, r)) context = 
+          if (typeExpr l context) = SOME Int andalso (typeExpr r context) = SOME Int then
+            SOME Bool
+          else 
+            NONE
+      | typeExpr (IfExpr(condition, thenBranch, elseBranch)) context = 
+          if (typeExpr condition context) = SOME(Bool) then
+            let 
+              val typeOfThen = typeExpr thenBranch context
+            in 
+              if typeOfThen = (typeExpr elseBranch context) then
+                typeOfThen
+              else
+                NONE
+            end
+          else
+            NONE
+      | typeExpr (VarExpr(v)) context = 
+          #2 (foldl (fn ((varName, varType), (isSome, optionType)) => 
+              if v = varName andalso not isSome then (true, SOME varType) 
+              else (isSome, optionType)
+            ) 
+            (false, NONE) 
+            (context)
+          )
+      | typeExpr (FunExpr(funName, paramName, paramType, returnType, body)) context = 
+          let 
+            val funPair = (funName, Arrow(paramType, returnType))
+            val paramPair = (paramName, paramType)
+            val currentContext = funPair::paramPair::context
+          in
+            case typeExpr body currentContext of
+              SOME(t) => 
+                if t = returnType then
+                  SOME(#2(funPair))
+                else 
+                  NONE
+              | NONE => NONE 
+          end
+      | typeExpr (ApplyExpr(func, arg)) context = 
+          case typeExpr func context of
+            SOME(Arrow(paramType, returnType)) =>
+              if (typeExpr arg context) = SOME(paramType) then
+                SOME returnType
+              else NONE
+            | SOME(_) => NONE 
+            | NONE => NONE
+
+  in 
+    typeExpr expr nil
   end;
+
+
+
