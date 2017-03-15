50bc4a9e523e4683907d54e5392e996f50b8a1c8
anwesht <anwesh.tuladhar@gmail.com>
2017-03-14 20:23:41 -0400

    Simple working as4

--- src/assignment4/as4.ml
+++ src/assignment4/as4.ml
@@ -71,7 +71,7 @@ fun tc expr =
     typeExpr expr nil
   end;
 
-fun eval expr = 
+(*fun eval expr = 
   let 
     fun betaAdd (IntExpr(x)) (IntExpr(y)) = IntExpr(x + y)
       | betaAdd _ _ = raise Stuck;
@@ -79,15 +79,26 @@ fun eval expr =
     fun betaLess (IntExpr(x)) (IntExpr(y)) = if x < y then TrueExpr else FalseExpr
       | betaLess _ _ = raise Stuck;
 
+    fun betaIfThenElse TrueExpr thenBranch _ = thenBranch
+      | betaIfThenElse FalseExpr _ elseBranch = elseBranch
+      | betaIfThenElse _ _ _ = raise Stuck;
+
+    fun betaApply (f as FunExpr(funName, paramName, paramType, returnType, body)) expr = 
+          ((sub f paramName body) handle 
+            Capture => raise Stuck)
+      | betaApply _ _ = raise Stuck;
+
     fun isTypeSafe e = 
       case tc e of 
         SOME _ => true
-        | NONE => false
+        | NONE => false;
 
     fun searchRule (t as TrueExpr) = t
       | searchRule (f as FalseExpr) = f
-      | searchRule (n as (IntExpr(_))) = n
-      | searchRule (pe as (PlusExpr(l, r))) = 
+      | searchRule (n as IntExpr(_)) = n
+      | searchRule (func as FunExpr(_,_,_,_,_)) = func
+      | searchRule (v as VarExpr(_)) = raise Stuck
+      | searchRule (pe as PlusExpr(l, r)) = 
           let 
             val rval = if isTypeSafe r then searchRule r else raise Stuck;
             val lval = if isTypeSafe l then searchRule l else raise Stuck
@@ -101,11 +112,82 @@ fun eval expr =
           in 
             betaLess lval rval
           end
-      | searchRule _ = raise NotFinished;
+      | searchRule (ie as IfExpr(condition, thenBranch, elseBranch)) = 
+          let
+            val condBool = if isTypeSafe condition then searchRule condition else raise Stuck
+          in
+            searchRule (betaIfThenElse condBool thenBranch elseBranch)
+          end
+      | searchRule (ae as ApplyExpr(func, arg)) = 
+          let 
+            val newExpr = if isTypeSafe func then betaApply func arg else raise Stuck
+          in
+            searchRule newExpr
+          end
+  in
+    searchRule expr
+  end;*)
+
+fun eval expr = 
+  let 
+    fun betaAdd (IntExpr(x)) (IntExpr(y)) = IntExpr(x + y)
+      | betaAdd _ _ = raise Stuck;
+
+    fun betaLess (IntExpr(x)) (IntExpr(y)) = if x < y then TrueExpr else FalseExpr
+      | betaLess _ _ = raise Stuck;
+
+    fun betaIfThenElse TrueExpr thenBranch _ = thenBranch
+      | betaIfThenElse FalseExpr _ elseBranch = elseBranch
+      | betaIfThenElse _ _ _ = raise Stuck;
+
+    fun betaApply (f as FunExpr(funName, paramName, paramType, returnType, body)) arg = 
+         (* ((sub f paramName body) handle 
+            Capture => raise Stuck)*)
+          (sub arg paramName (sub f funName body) )
+            (*handle Capture => raise Stuck)*)
+      | betaApply _ _ = raise Stuck;
+
+    fun isTypeSafe e = 
+      case tc e of 
+        SOME _ => true
+        | NONE => false;
+
+    fun searchRule (t as TrueExpr) = t
+      | searchRule (f as FalseExpr) = f
+      | searchRule (n as IntExpr(_)) = n
+      | searchRule (func as FunExpr(_,_,_,_,_)) = func
+      | searchRule (v as VarExpr(_)) = raise Stuck
+      | searchRule (pe as PlusExpr(l, r)) = 
+          let 
+            val rval = searchRule r;
+            val lval = searchRule l
+          in 
+            betaAdd lval rval
+          end
+      | searchRule (le as LessExpr(l, r)) = 
+          let 
+            val rval = searchRule r;
+            val lval = searchRule l
+          in 
+            betaLess lval rval
+          end
+      | searchRule (ie as IfExpr(condition, thenBranch, elseBranch)) = 
+          let
+            val condBool = searchRule condition
+          in
+            searchRule (betaIfThenElse condBool thenBranch elseBranch)
+          end
+      | searchRule (ae as ApplyExpr(func, arg)) = 
+          let 
+            val newExpr = betaApply func arg
+            (*val newExpr = if isTypeSafe func then betaApply func arg else raise Stuck*)
+          in
+            if isTypeSafe newExpr then searchRule newExpr else raise Stuck
+          end
+      (*| searchRule _ = raise NotFinished;*)
   in
     searchRule expr
   end;
-
 
 
 
