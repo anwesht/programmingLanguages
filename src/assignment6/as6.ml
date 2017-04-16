c6c6f44de464fde2bd7f1e3f6ead9ee6bce6848a
anwesht <anwesh.tuladhar@gmail.com>
2017-04-15 21:42:02 -0400

    Simple type checker for algebraic types working.

---
+++ src/assignment6/as6.ml
@@ -0,0 +1,318 @@
+(*
+ * Created by atuladhar on 04/15/17.
+ * Pledge: I pledge my Honor that I have not cheated, and will not cheat, on this assignment.
+ * Name: Anwesh Tuladhar
+ *)
+
+(** Import diMLazy-Au to utilise the definition on DiMLazy datatypes, expressions and sub function. *)
+use "diMLazy-Au.sml";
+
+(** Type checker for DiMLazy expressions. 
+  * @param expr => the expression to be type checked
+  * @returns => option of datatype typ. The NONE option is returned iff the expression is ill typed.
+  *)
+fun tc expr = 
+  let 
+    (** Look for the given variable within the given context 
+      * @param var => the variable to search for
+      * @param contexts => list of pairs of varName and varType in current context.
+      * @returns => option of dataype typ representing the type of var.
+                 => NONE if the variable is not found in the given context.
+      *)
+    fun lookupVar (var:string) nil = NONE
+      | lookupVar var ((varName, varType)::contexts) = 
+          if var = varName then SOME varType else lookupVar var contexts;
+
+    (** Type check the given expression within the context provided.
+      * @param expr => the expression to be type checked.
+      * @param context => the context within which to type check expr. The context is initialised to nil
+          and is augmented by new variables only by the expression FunExpr.
+      * @returns => option of datatype typ. The NONE option is returned iff the expression is ill typed.
+      *)
+    fun typeExpr TrueExpr _ = SOME Bool
+      | typeExpr FalseExpr _ = SOME Bool
+      | typeExpr (IntExpr(_)) _ = SOME Int
+      | typeExpr (PlusExpr(l, r)) context = 
+          if (typeExpr l context) = SOME Int andalso (typeExpr r context) = SOME Int then
+            SOME Int
+          else 
+            NONE
+      | typeExpr (LessExpr(l, r)) context = 
+          if (typeExpr l context) = SOME Int andalso (typeExpr r context) = SOME Int then
+            SOME Bool
+          else 
+            NONE
+      | typeExpr (IfExpr(condition, thenBranch, elseBranch)) context = 
+          if (typeExpr condition context) = SOME(Bool) then
+            let 
+              val typeOfThen = typeExpr thenBranch context
+            in 
+              if typeOfThen = (typeExpr elseBranch context) then
+                typeOfThen
+              else
+                NONE
+            end
+          else
+            NONE
+      | typeExpr (VarExpr(v)) context = 
+          lookupVar v context
+
+      | typeExpr (FunExpr(funName, paramName, paramType, returnType, body)) context = 
+          let 
+            val funPair = (funName, Arrow(paramType, returnType));
+            val paramPair = (paramName, paramType);
+            val currentContext = funPair::paramPair::context
+          in
+            case typeExpr body currentContext of
+              SOME(t) => 
+                if t = returnType then
+                  SOME(#2(funPair))
+                else 
+                  NONE
+              | NONE => NONE 
+          end
+      | typeExpr (ApplyExpr(func, arg)) context = 
+          (
+            case typeExpr func context of
+            SOME(Arrow(paramType, returnType)) =>
+              if (typeExpr arg context) = SOME(paramType) then
+                SOME returnType
+              else NONE
+            | SOME(_) => NONE 
+            | NONE => NONE
+          )
+
+      | typeExpr UnitExpr _ = SOME Unit
+      | typeExpr (PairExpr(e1, e2)) context = 
+          let
+            val t1 = typeExpr e1 context
+            val t2 = typeExpr e2 context
+          in
+            case t1 of 
+              SOME(tt1) =>
+                (
+                  case t2 of 
+                    SOME(tt2) => 
+                      SOME(Prod(tt1, tt2))
+                    | NONE => NONE
+                )
+              | NONE => NONE
+          end
+      | typeExpr (FstExpr(pair)) context = 
+          (
+            case typeExpr pair context of 
+            SOME(Prod(fst, _)) => SOME(fst)
+            | _ => NONE
+          )
+      | typeExpr (SndExpr(pair)) context =
+          (
+            case typeExpr pair context of
+            SOME(Prod(_, snd)) => SOME(snd)
+            | _ => NONE
+          )
+      | typeExpr (SumExpr(side, e, t as Sum(l, r))) context =
+          let 
+            val eType = typeExpr e context
+          in
+            (
+              case side of 
+                Left => 
+                  (
+                    case eType of 
+                      SOME(l) => SOME(t)
+                      | _ => NONE
+                  )
+                | Right =>
+                  (
+                    case eType of
+                      SOME(r) => SOME(t)
+                      | _ => NONE
+                  )
+            )
+          end
+      | typeExpr (CaseExpr(e, x, e1, y, e2)) context =
+          (case typeExpr e context of
+              SOME(Sum(l, r)) => 
+                let 
+                  val e1Context = (x, l)::context
+                  val e2Context = (y, r)::context
+                  val e1Type = typeExpr e1 e1Context
+                  val e2Type = typeExpr e2 e2Context
+                in
+                  case e2Type of
+                    SOME(t2) => 
+                      (
+                        case e1Type of 
+                        SOME(t1) => if t1 = t2 then SOME(t1) else NONE
+                        | NONE => NONE
+                      )
+                    | NONE => NONE
+                end
+              | _ => NONE
+          )
+      | typeExpr other _ = NONE
+  in 
+    typeExpr expr nil
+  end;
+
+
+
+val t = TrueExpr;
+val f = FalseExpr;
+val i = IntExpr(3);
+val v = VarExpr("variable");
+val p = PlusExpr(VarExpr("a"), VarExpr("b"));
+val p1 = PlusExpr(IntExpr(3), IntExpr(4));
+val l = LessExpr(VarExpr("a"), VarExpr("b"));
+val l1 = LessExpr(IntExpr(3), IntExpr(4));
+val ifSimple = IfExpr(TrueExpr, IntExpr(2), IntExpr(3));
+
+
+val ifBad = IfExpr(TrueExpr, IntExpr(2), TrueExpr);
+
+
+val ifBad1 = IfExpr(IntExpr(1), TrueExpr, FalseExpr);
+val ifExp = IfExpr(LessExpr(VarExpr("a"), IntExpr(5)), 
+  PlusExpr(VarExpr("a"), IntExpr(10)), PlusExpr(IntExpr(5), IntExpr(10)));
+val funExp = FunExpr("f", "a", Int, Int, ifExp);
+val applyFunExp1 = ApplyExpr(funExp, IntExpr(3));
+val applyFunExp2 = ApplyExpr(funExp, IntExpr(7));
+val funExp1 = FunExpr("f", "unused", Int, Int, ifSimple);
+
+
+val funExp2 = FunExpr("f", "a", Int, Int, ifBad);   
+
+
+(*Evaluates even if not typesafe as condition always true.*)
+val ifForFun = IfExpr(LessExpr(VarExpr("a"), IntExpr(5)), VarExpr("a"), IntExpr(5));  (*returns 5 or less*)
+val funExp3 = FunExpr("f", "a", Int, Int, ifForFun);
+val applyFunExp3 = ApplyExpr(funExp3, IntExpr(99));
+val applyFunExp3_1 = ApplyExpr(funExp3, IntExpr(2));
+val funIdentity = FunExpr("f", "a", Int, Int, VarExpr("a"));
+val applyIdentity1 = ApplyExpr(funIdentity, funExp3);
+val applyIdentity2 = ApplyExpr(funIdentity, TrueExpr);
+
+val sumBody = IfExpr(
+  LessExpr(
+    VarExpr("x"),
+    IntExpr(2)
+  ), 
+  IntExpr(1), 
+  PlusExpr(
+    VarExpr("x"),
+    ApplyExpr(
+      VarExpr("sum"),
+      PlusExpr(
+        VarExpr("x"),
+        IntExpr(~1)
+      )
+    )
+  )
+)
+
+val sum = FunExpr("sum", "x", Int, Int, sumBody);
+val applySum = ApplyExpr(sum, IntExpr(10)); (*Sum = 55*)
+
+val sumOf10 = FunExpr("f", "unused", Int, Int, applySum);
+val applyUnusedBadArg = ApplyExpr(sumOf10, LessExpr(TrueExpr, FalseExpr));    (*Shows Call By Name evaluation strategy*)
+
+val cbnEval = PlusExpr(VarExpr("bad"), ApplyExpr(sum, IntExpr(10))); (*Can be verified by using print*)
+(*val applyCBN = ApplyExpr(cbnEval, VarExpr("BAD"));*)
+
+
+val f2 = ApplyExpr(FunExpr("loop", "x", Int, Int, ApplyExpr(VarExpr("loop"), VarExpr("x"))), IntExpr(5));   (*Infinite loop*)
+val f3 =  FunExpr(
+            "f", "x", Int, Int, 
+            PlusExpr(
+              VarExpr("x"),   (*false*)
+              ApplyExpr(  (*false*)
+                FunExpr("f", "x", Int, Int,  (*false*)
+                  PlusExpr( (*false orelse true = true*)
+                    ApplyExpr(
+                      FunExpr("f", "z", Int, Int,   (*false*)
+                        ApplyExpr(VarExpr("f"), VarExpr("x"))
+                      ), 
+                      ApplyExpr(VarExpr("f"), VarExpr("x")) (*true*)
+                    ), 
+                    (*VarExpr("z"))*) (*Free variable = ill typed.*)
+                    IntExpr(99)) (*Free variable = ill typed.*)
+                ),
+                ApplyExpr(
+                  VarExpr("f"), 
+                  VarExpr("x")
+                )
+              )
+            )
+          );
+
+val applyF3 = ApplyExpr(f3, IntExpr(3)); (*Stuck as free variable = ill typed*)
+
+val f4 = FunExpr(
+          "f", "x", Int, Int, 
+          PlusExpr(
+            VarExpr("x"),
+            VarExpr("y")
+          )
+        );
+val f5 = FunExpr(
+          "f", "z", Int, Int, 
+          PlusExpr(
+            VarExpr("z"),
+            VarExpr("y")
+          )
+        );
+
+val f6 = FunExpr(
+          "f", "x" , Int, Bool, 
+          ApplyExpr(
+            FunExpr(
+              "f", "x", Bool, Bool, 
+              ApplyExpr(VarExpr("f"), VarExpr("x"))
+            ),
+            IntExpr(5)
+          )
+        );
+
+val f7 = FunExpr(
+          "f", "x" , Int, Bool, 
+          ApplyExpr(VarExpr("f"), VarExpr("x"))
+        );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/assignment6/diMLazy-Au.sml
@@ -0,0 +1,163 @@
+(* This file defines several diMLazy-Au datatypes and some example expressions. *)
+
+(* diMLazy-Au types *)
+datatype typ = Bool | Int | Arrow of typ * typ | Unit
+             | Prod of typ * typ | Sum of typ * typ
+             | Var of string | Rec of string * typ; (* Var and Rec are grad level *)
+
+datatype side = Left | Right;
+
+(* diMLazy-Au expressions *)
+datatype expr = TrueExpr | FalseExpr | IntExpr of int
+ | PlusExpr of expr*expr | LessExpr of expr*expr
+ | IfExpr of expr*expr*expr (* i.e., IfExpr(condition, thenBranch, elseBranch) *)
+ | VarExpr of string | ApplyExpr of expr*expr (* i.e., ApplyExpr(fun, arg) *)
+ | FunExpr of string*string*typ*typ*expr
+ (* i.e., FunExpr(functionName, parameterName, parameterType, returnType, body) *)
+ | UnitExpr | PairExpr of expr*expr
+ | FstExpr of expr (* selects first element of a pair *) | SndExpr of expr
+ | SumExpr of side*expr*typ
+ (* e.g., "(inl 5):int+unit" would be SumExpr(Left,IntExpr(5),Sum(Int,Unit)) *)
+ | CaseExpr of expr*string*expr*string*expr
+ (* e.g., "case e of (inl x=>e1 | inr y=>e2)" would be CaseExpr(e,x,e1,y,e2) *)
+ | RollExpr of expr | UnrollExpr of expr; (* RollExpr and UnrollExpr are grad level *)
+
+(* Additional datatypes and exception, to help define evaluation of diMLazy-Au expressions *)
+
+datatype ctxt = Hole | PlusCtxt1 of expr*ctxt | PlusCtxt2 of ctxt*int
+ | LessCtxt1 of expr*ctxt | LessCtxt2 of ctxt*int
+ | IfCtxt of ctxt*expr*expr  | ApplyCtxt of ctxt*expr
+ | PairCtxt1 of expr*ctxt | PairCtxt2 of ctxt*expr (* where expr is a value *)
+ | FstCtxt of ctxt | SndCtxt of ctxt | SumCtxt of side*ctxt*typ
+ | CaseCtxt of ctxt*string*expr*string*expr
+ | RollCtxt of ctxt | UnrollCtxt of ctxt;
+
+exception Stuck;
+
+
+(* The remainder of this file contains example diMLazy-Au expressions *)
+
+(* Tell SML/NJ to print these expressions in detail *)
+Control.Print.printDepth := 30;
+
+(* Define e1 to represent:
+   fun f(x:unit+(int*int)):(int*int)+unit =
+     case x of inl x1=> (inr x1):(int*int)+unit
+             | inr x2=> (inl (x2.snd,x2.fst)):(int*int)+unit
+*)
+val e1 = let val t1 = Sum(Unit,Prod(Int,Int))
+             val t2 = Sum(Prod(Int,Int),Unit)
+         in
+           FunExpr("f","x",t1,t2,
+             CaseExpr(VarExpr("x"),
+                      "x1",
+                      SumExpr(Right,VarExpr("x1"),t2),
+                      "x2",
+                      SumExpr(Left, PairExpr(SndExpr(VarExpr("x2")),
+                                             FstExpr(VarExpr("x2"))), 
+                              t2)))
+         end;
+
+(* Define e2 to represent:
+   (inr (if false then 4 else 5, 6)):unit+(int*int)
+*)
+val e2 = SumExpr(Right, 
+                 PairExpr(IfExpr(FalseExpr,IntExpr(4),IntExpr(5)),IntExpr(6)),
+                 Sum(Unit,Prod(Int,Int)));
+
+(* Define e3 to represent:
+   case e1(e2) of inl z1=> z1.snd end
+                | inr z2=> 88
+*)
+val e3 = CaseExpr(ApplyExpr(e1,e2),
+                  "z1",
+                  SndExpr(VarExpr("z1")),
+                  "z2",
+                  IntExpr(88));
+
+(* Define rType (rolled-list type) to represent mu t.unit+(int*t) *)
+val rType = Rec("t",Sum(Unit,Prod(Int,Var("t"))));
+
+(* Define uType (unrolled-list type) to represent unit+(int* mu t.unit+(int*t)) *)
+val uType = Sum(Unit,Prod(Int,Rec("t",Sum(Unit,Prod(Int,Var("t"))))));
+
+(* Define e4 to represent:
+   fun reverse(L:rType):rType =
+     (fun revDiffLists(Ls:rType*rType):rType =
+        case unroll(Ls.fst)
+          of inl empty => Ls.snd
+           | inr nonempty => revDiffLists(nonempty.snd,
+                                          roll((inr(nonempty.fst,Ls.snd)):uType)
+     )
+     (L, roll((inl ()):uType))
+   ---
+   With ML's syntactic sugar, we could write e4 as:
+   fun reverse(L) = 
+     let fun revDiffLists(nil,done)=done
+           | revDiffLists(x::xs,done)=revDiffLists(xs, x::done)
+     in revDiffLists(L,nil)
+     end
+*)
+val e4 =
+  let
+    val argExpr = PairExpr(SndExpr(VarExpr("nonempty")),
+                           RollExpr(SumExpr(Right, 
+                                            PairExpr(FstExpr(VarExpr("nonempty")),
+                                                     SndExpr(VarExpr("Ls"))),
+                                            uType)))
+    val lastBody = ApplyExpr(VarExpr("revDiffLists"),argExpr)
+    val rDLBody = CaseExpr(UnrollExpr(FstExpr(VarExpr("Ls"))),
+                           "empty", SndExpr(VarExpr("Ls")),
+                           "nonempty",lastBody)
+    val f1 = FunExpr("revDiffLists","Ls",Prod(rType,rType),rType,rDLBody)
+    val e = PairExpr(VarExpr("L"),RollExpr(SumExpr(Left,UnitExpr,uType)))
+  in
+    FunExpr("reverse","L",rType,rType,ApplyExpr(f1,e)) 
+  end;
+
+(* Define e5 to represent 2::3::4::nil, or in diMLazy-Au:
+   roll( (inr(2,
+              roll( (inr(3,
+                         roll( (inr (4, 
+                                     roll( (inl ()):uType )
+                                    )
+                               ):uType
+                             )
+                        )
+                     ):uType
+                  )
+             )
+         ):uType
+       )
+*)
+val e5 = 
+  RollExpr(
+    SumExpr(Right,
+            PairExpr(
+              IntExpr(2),
+              RollExpr(
+                SumExpr(Right,
+                        PairExpr(
+                          IntExpr(3),
+                          RollExpr(
+                            SumExpr(Right,
+                                    PairExpr(
+                                      IntExpr(4),
+                                      RollExpr(
+                                        SumExpr(Left,UnitExpr,uType))),
+                                    uType))),
+                        uType))),
+            uType));
+
+(* Define e6 to represent e4(e5) *)
+val e6 = ApplyExpr(e4,e5);
+
+val t1 = UnitExpr;
+val t2 = PairExpr(IntExpr(1), PairExpr(IntExpr(2), IntExpr(3)));
+val t3 = FstExpr(t2);
+val t4 = SndExpr(t2);
+val t5 = SumExpr(Left, 
+            PairExpr(IntExpr(5), IntExpr(6)),
+            Sum(Int, Int));
+val t6 = CaseExpr(t5, "x", TrueExpr, "y", FalseExpr);
+
